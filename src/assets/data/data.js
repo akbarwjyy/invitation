ff9a10a52629dbcce64a7755a73155e26ec009e5
Akbar Wijaya <151990608+akbarwjyy@users.noreply.github.com>
2025-07-31 18:50:08 +0700

    initial commit

---
+++ src/assets/data/data.js
@@ -0,0 +1,126 @@
+export const data = {
+  bride: {
+    L: {
+      id: 1,
+      name: "Rika Pratiwi",
+      child: "Putri",
+      father: "Haryana",
+      mother: "Ningsih",
+      image: "./src/assets/images/cowok.jpg",
+    },
+    P: {
+      id: 2,
+      name: "Arif Kurniawan",
+      child: "Putra",
+      father: "Wagiyo (Alm)",
+      mother: "Ngadinem",
+      image: "./src/assets/images/cewek.jpg",
+    },
+
+    couple: "./src/assets/images/prewed.jpg",
+  },
+
+  time: {
+    marriage: {
+      year: "2025",
+      month: "September",
+      date: "14",
+      day: "Minggu",
+      hours: {
+        start: "07.00",
+        finish: "Selesai",
+      },
+    },
+    reception: {
+      year: "2025",
+      month: "September",
+      date: "14",
+      day: "Minggu",
+      hours: {
+        start: "12.00",
+        finish: "14.00",
+      },
+    },
+    address: "Bongsren RT 07, Gilangharjo, Pandak, Bantul",
+  },
+
+  link: {
+    calendar: "https://calendar.app.google/HP5B7nHS9tmKkcvf9",
+    map: "https://maps.app.goo.gl/3k5pwJrjAKMLETdD7",
+  },
+
+  galeri: [
+    {
+      id: 1,
+      image: "./src/assets/images/1.png",
+    },
+    {
+      id: 2,
+      image: "./src/assets/images/2.png",
+    },
+    {
+      id: 3,
+      image: "./src/assets/images/3.png",
+    },
+    {
+      id: 4,
+      image: "./src/assets/images/4.png",
+    },
+    {
+      id: 5,
+      image: "./src/assets/images/5.png",
+    },
+  ],
+
+  bank: [
+    {
+      id: 1,
+      name: "Rika",
+      icon: "./src/assets/images/bca.png",
+      rekening: "12345678",
+    },
+    {
+      id: 2,
+      name: "Arif",
+      icon: "./src/assets/images/bri.png",
+      rekening: "12345678",
+    },
+  ],
+
+  audio: "./src/assets/audio/wedding.mp3",
+
+  api: "https://script.google.com/macros/s/AKfycbwsmz3Cq-DPGefVrJiTUKz4CsWrq3AnU1WIjQBezpyz2KFjZrCvYSrarc9zrd6eQmDjMQ/exec",
+
+  navbar: [
+    {
+      id: 1,
+      teks: "Home",
+      icon: "bx bxs-home-heart",
+      path: "#home",
+    },
+    {
+      id: 2,
+      teks: "Mempelai",
+      icon: "bx bxs-group",
+      path: "#bride",
+    },
+    {
+      id: 3,
+      teks: "Tanggal",
+      icon: "bx bxs-calendar-check",
+      path: "#time",
+    },
+    {
+      id: 4,
+      teks: "Galeri",
+      icon: "bx bxs-photo-album",
+      path: "#",
+    },
+    {
+      id: 5,
+      teks: "Ucapan",
+      icon: "bx bxs-message-rounded-dots",
+      path: "#wishas",
+    },
+  ],
+};
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { home } from "./js/home.js";
+import { bride } from "./js/bride.js";
+import { time } from "./js/time.js";
+import { galeri } from "./js/galeri.js";
+import { wishas } from "./js/wishas.js";
+import { navbar } from "./js/navbar.js";
+import { welcome } from "./js/welcome.js";
+// src/js/main.js
+// src/js/main.js
+
+// load content
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init();
+
+  welcome();
+  navbar();
+  home();
+  bride();
+  time();
+  galeri();
+  wishas();
+});
